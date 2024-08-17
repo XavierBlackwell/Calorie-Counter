72872a4eea565a107f3e2b909e29e10b10aa2332
jvon <xavieflash@live.com>
2024-08-17 00:55:48 -0500

    create new html after adding entry

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetId = '#' + entryDropdown.value;
-  const targetInputContainer = document.querySelector(targetId + ' .input-container');
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
+  `;
 }
