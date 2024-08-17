7b8f43f77f3a16720288c2aa63f52281c60d1cbd
jvon <xavieflash@live.com>
2024-08-16 23:53:07 -0500

    add new functions for regex

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+let isError = false;
+
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+  const regex = /e/i;
+}
